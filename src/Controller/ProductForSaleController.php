05caa9614690160a9cd2c6552825ae0ddd4169f3
boudidah ilyes <ilyes.boudidah@esprit.tn>
2024-02-14 21:21:02 +0100

    fixed some problems in front Product forsale and forrent

--- src/Controller/ProductForSaleController.php
+++ src/Controller/ProductForSaleController.php
@@ -3,43 +3,29 @@
 namespace App\Controller;
 
 use App\Entity\Member;
-use App\Entity\ProductCategory;
-use App\Entity\ProductForRent;
 use App\Entity\ProductForSale;
-use App\Entity\ProductForTrade;
 use App\Entity\ProductImage;
-use App\Form\ProductCategoryFormType;
 use App\Form\ProductForSaleFormType;
-use App\Repository\ProductCategoryRepository;
-use App\Repository\ProductForRentRepository;
-use App\Repository\ProductForSaleRepository;
-use App\Repository\ProductForTradeRepository;
-use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Vich\UploaderBundle\Exception\VichUploaderExceptionInterface;
 
-class ProductController extends AbstractController
+class ProductForSaleController extends AbstractController
 {
     private $entityManager;
-    private $productForSaleRepository;
-    private $productForTradeRepository;
-    private $productForRentRepository;
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
     }
-    //ProductForSale Begin
+
+    //-------------------------ProductForSale Begin----------------------
     #[Route('/productForSale', name: 'app_product_for_sale')]
     public function index(): Response
     {
         $listProduct = $this->entityManager->getRepository(ProductForSale::class)->findAll();
-        return $this->render('product/index.html.twig', [
+        return $this->render('product/frontOfficeListProductForSale.html.twig', [
             'listProduct' => $listProduct
         ]);
     }
@@ -67,9 +53,18 @@ class ProductController extends AbstractController
             $this->entityManager->flush();
             return $this->redirectToRoute('app_product_for_sale');
         }
-        return $this->render('product/addProductForSale.html.twig', ['formProduct' => $form->createView()]);
+        return $this->render('product/frontOfficeAddProductForSale.html.twig', ['formProduct' => $form->createView()]);
     }
 
+    #[Route('/productForSale/overview/{id}', name: 'app_product_for_sale_details')]
+    public function ProductForSaleMoreDetails($id): Response
+    {
+        $product = $this->entityManager->getRepository(ProductForSale::class)->findOneBy(['id' => $id]);
+        return $this->render('product/frontOfficeProductForSaleDetails.html.twig', [
+            'product' => $product
+        ]);
+    }
+    
     #[Route('/homeDashboard/productForSale', name: 'app_product_for_sale_list')]
     public function listProductForSale(): Response
     {
@@ -83,7 +78,7 @@ class ProductController extends AbstractController
     public function overviewProductForSale($id): Response
     {
         $product = $this->entityManager->getRepository(ProductForSale::class)->findOneBy(['id' => $id]);
-        return $this->render('product/backOfficeProductOverView.html.twig', [
+        return $this->render('product/backOfficeProductForSaleOverView.html.twig', [
             'product' => $product
         ]);
     }
@@ -123,5 +118,5 @@ class ProductController extends AbstractController
         return $this->redirectToRoute('app_product_for_sale_list');
         
     }
-    //ProductForSale End
+    //-----------------------------ProductForSale End-------------------------------------------
 }
